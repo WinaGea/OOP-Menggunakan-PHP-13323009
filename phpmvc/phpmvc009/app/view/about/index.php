bbe071cf5e72c6519b89c26f6e70e86ecd369880
WinaGea <151107459+WinaGea@users.noreply.github.com>
2024-04-13 19:59:46 +0700

    Create index.php

---
+++ phpmvc/phpmvc009/app/view/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>\img\13323009_Wina_Sorta_Maria_Gea_D3TK.jpeg" alt="Wina Gea" width="180" class="rounded-circle shadow">
+    <p>Hai nama saya <?= $data['nama']; ?> pekerjaan saya adalah <?= $data['pekerjaan']; ?> umur saya <?= $data['umur']; ?></p>
+</div>
+
